c4430bb592109a846625ee33584076ecb452eb6a
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-06 11:11:40 +0200

    class Movie: added variables - rating, oscars class MovieLibrary: fixed

--- src/MovieLibrary.java
+++ src/MovieLibrary.java
@@ -6,12 +6,13 @@ class MovieLibrary {
                 "z niedowładem kończyn, który staje się miliarderem i bohaterem wojny w Wietnamie.";
         movie.director = "Robert Zemeckis";
         movie.rating = 9;
+        movie.oscars = true;
 
         System.out.println("Informacje o filmie:");
-        System.out.printf("Tytuł: %s\n", movie.title);
-        System.out.printf("Opis: %s\n", movie.description);
-        System.out.printf("Reżyser: %s\n", movie.director);
-        System.out.printf("Ocena: %f/10\n", movie.rating);
-        System.out.printf("Czy otrzymał Oskara: %s\n", movie.oscars);
+        System.out.println("Tytuł: " + movie.title);
+        System.out.println("Opis: " + movie.description);
+        System.out.println("Reżyser: " + movie.director);
+        System.out.println("Ocena: " + movie.rating + "/10");
+        System.out.println("Czy otrzymał Oskara: " + movie.oscars);
     }
 }
